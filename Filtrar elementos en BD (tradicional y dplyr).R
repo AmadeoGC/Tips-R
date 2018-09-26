1432e831876e069a1f5c4561a3df1f63af776a91
Amadeo Guzman <40497655+AmadeoGC@users.noreply.github.com>
2018-09-26 11:18:29 -0300

    Update Filtrar elementos en BD (tradicional y dplyr).R

--- Filtrar elementos en BD (tradicional y dplyr).R
+++ Filtrar elementos en BD (tradicional y dplyr).R
@@ -1,8 +1,8 @@
 #######################################################
 ###  Formas de filtrar elementos en bases de datos  ###
 ###       diferencias entre OR (|) y AND (&)        ###
-###           método clásico y dplyr                ###
-###            <Amadeo Guzmán C.>                   ###
+###            método clásico y dplyr               ###
+###             <Amadeo Guzmán C.>                  ###
 #######################################################
 
 #datos
@@ -12,21 +12,33 @@ var2 <- c(1,2,4,5,15)
 bdatos <- data.frame(var1, var2)
 bdatos
 
-
 library(dplyr)
 
-## Filtrar datos dentro de una columna -> OR = |
+
+#-----------------------------------------------------------------------
+##  OR = | (usar principalmente para filtrara dentro de una columna)
+#-----------------------------------------------------------------------
 #método clásico
-bdatos[bdatos$var1=="b" | bdatos$var1=="d",]
-#dplyr
+bdatos[bdatos$var1=="b" | bdatos$var1=="d",] #en una misma columna
+bdatos[bdatos$var1=="b" | bdatos$var2==1,]  #en diferentes variables
+
+#dplyr (filter)
 bdatos %>% 
   filter(var1=="b" | var1=="d")
 
+bdatos %>% 
+  filter(var1=="b" | var2==1)
 
-## Filtrar datos con criterios de diferentes columnas  -> AND = &
+
+#--------------------------------------------------------------------
+##  AND = & (filtrar datos con criterios de DIFERENTES columnas)
+#--------------------------------------------------------------------
 #método clásico
 bdatos[bdatos$var1=="b" & bdatos$var2==15,]
-#dplyr
+
+#dplyr (filter)
 bdatos %>% 
   filter(var1=="b" & var2==15)
-
+#Utilizar la coma "," entre cada condición es equivalente a la expresión "&"
+bdatos %>% 
+  filter(var1=="b", var2==15)
